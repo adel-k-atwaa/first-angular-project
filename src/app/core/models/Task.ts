6fd5991ad4c67dee15424b540d0252c4b2643338
Adel Kamal <adelkamal911@gmail.com>
2025-01-25 13:48:09 +0200

    define global classess

---
+++ src/app/core/models/Task.ts
@@ -0,0 +1,16 @@
+export class Task{
+  id:string;
+  userId:string;
+  title:string;
+  summary:string;
+  dueDate:string;
+
+  constructor(){
+    this.id = "";
+    this.userId = "";
+    this.title = "";
+    this.summary = "";
+    this.dueDate = "";
+  }
+
+}
